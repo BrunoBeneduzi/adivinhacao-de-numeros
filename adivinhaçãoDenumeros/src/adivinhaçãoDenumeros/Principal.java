9bee9b6a4a6aeecee542632d452ab9e928379bb7
Bruno Beneduzi <124108326+BrunoBeneduzi@users.noreply.github.com>
2023-07-25 09:24:11 -0300

    projeto para adivinhar um numero
    gera um numero aleatório e deve acertar qual numero foi gerado, o programa vai falando se você colocou um numero muito acima ou muito abaixo, são 10 tentativas.

---
+++ adivinhaçãoDenumeros/src/adivinhaçãoDenumeros/Principal.java
@@ -0,0 +1,30 @@
+package adivinhaçãoDenumeros;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Random;
+public class Principal { 
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	Random random = new Random();
+	int sorteado, numero;
+	
+	sorteado = random.nextInt(100);
+	System.out.println("Bem vindo ao jogo de adivinhação, o computador vai gerar um numero aleatorio de 0 até 100, tenta acertar, você tem 10 tentativas");
+	
+	for (int i = 0; i < 10; i++) {
+		System.out.print("Tentativas numero "+ (i+1) +" Digite o numero que foi sorteado -> ");
+		numero = sc.nextInt();
+		System.out.println();
+		
+		if (numero == sorteado) {
+			System.out.println("****ACERTOU******");
+			break;
+		}
+		else if (numero > sorteado) System.out.println("um pouco menos <<<<<");
+		else if (numero < sorteado) System.out.println("Um pouco mais >>>>>");
+		
+		if (i == 9) System.out.println("Maximo de tentativas feitas, o numeoro era o " + sorteado);	
+	}	
+	}
+}
